f1046b7c77b781a802dec4a01e1d1d6ebdd81af9
Kankan912 <kankanway912@gmail.com>
2025-08-29 20:14:14 +0100

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -4,11 +4,11 @@ import type { Database } from './types';
 
 const SUPABASE_URL = "https://piyvinbuxpnquwzyugdj.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpeXZpbmJ1eHBucXV3enl1Z2RqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1NDAzMDMsImV4cCI6MjA3MTExNjMwM30.khqzhiITwYsgbZBVI5X-zIVeGkNOD6tGy8eQaCU-k4E";
-
+const API_KEY = "sb_publishable_DF22swxDJdIeuGuIFqMMNg_kzx3a0bC"
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY,API_KEY{
   auth: {
     storage: localStorage,
     persistSession: true,
